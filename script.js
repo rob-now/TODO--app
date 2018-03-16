a26191141838551e018a358f029abb2c08b82080
rob-now <rnowakowski.prv@gmail.com>
2018-03-16 11:27:00 +0100

    Adding tasks to the list

---
+++ script.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const todoForm = document.querySelector("#todo-form");
+    const search = document.querySelector("#todoSearch");
+    const todoList = document.querySelector("#todoList");
+
+    function addTask(text) {
+        console.log("Adding task");
+        // to-do element div
+        var todoElement = document.createElement("div");
+        todoElement.classList.add("todo-element");
+
+        // bar div
+        var todoElementBar = document.createElement("div");
+        todoElementBar.classList.add("todo-element-bar");
+
+        // heading
+        var heading = document.createElement("h4");
+        heading.classList.add("todo-element-date");
+        // assigning my date format to heading
+        heading.innerText = myDateFormat();
+
+        // delete button
+        var deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("todo-element-delete");
+        deleteBtn.title = "Delete task";
+        var deleteIcon = document.createElement("i");
+        deleteIcon.classList.add("far", "fa-trash-alt", "fa-lg");
+
+        // text div
+        var todoText = document.createElement("div");
+        todoText.classList.add("todo-element-text");
+
+        // adding elements to DOM
+        var todoElementDOM = todoList.appendChild(todoElement);
+        // bar area
+        var todoElementBarDOM = todoElementDOM.appendChild(todoElementBar);
+        todoElementBarDOM.appendChild(heading);
+        todoElementBarDOM.appendChild(deleteBtn).appendChild(deleteIcon);
+        // text area
+        var todoTextDOM = todoElementDOM.appendChild(todoText);
+        todoTextDOM.innerText = text;
+
+        // setting up my own date format
+        function myDateFormat() {
+            const currentDate = new Date();
+            var currentYear = currentDate.getFullYear();
+            var currentMonth = currentDate.getMonth() + 1;
+            var currentDay = currentDate.getDate();
+            var currentHour = currentDate.getHours();
+            var currentMinute = currentDate.getMinutes();
+
+            if (currentMonth < 10) {
+                currentMonth = "0" + currentMonth;
+            }
+            if (currentDay < 10) {
+                currentDay = "0" + currentDay;
+            }
+            if (currentHour < 10) {
+                currentHour = "0" + currentHour;
+            }
+            if (currentMinute < 10) {
+                currentMinute = "0" + currentMinute;
+            }
+            return currentYear
+                + "-"
+                + currentMonth
+                + "-"
+                + currentDay
+                + ", "
+                + currentHour
+                + ":"
+                + currentMinute;
+        }
+    }
+
+    document.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const textArea = this.querySelector(".task-area-text");
+        if (textArea.value !== "") {
+            addTask(textArea.value);
+            textArea.value = "";
+        }
+
+
+    })
+});
